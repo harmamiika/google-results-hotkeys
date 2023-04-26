4f0d721c13fd84d04d630981675d50cdaa908c03
Miika Härmä <harmamiika@gmail.com>
2023-04-26 14:52:23 +0300

    clean up code

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
-console.log('Hello World');
+const getLinks = () => {
+  const links = document.querySelectorAll('a > h3');
+  return Array.from(links).map((link) => link.parentElement);
+};
 
 window.onkeydown = (event) => {
-  if (document.activeElement.tagName === 'INPUT') return;
-  console.log('onkeydonw after input check');
-  // TODO: return if user has input field focused
+  if (['TEXTAREA', 'INPUT'].includes(document.activeElement.tagName)) return;
 
-  if (event.keyCode >= 48 && event.keyCode <= 57) {
-    console.log('number key');
-    const pressedNumber = event.keyCode - 48;
-    console.log(pressedNumber);
-
-    const links = document.querySelectorAll('a > h3');
-    console.log(links, 'links');
-    const parentLinks = Array.from(links).map((link) => link.parentElement);
-    console.log(parentLinks, 'parentLinks');
-
-    console.log(pressedNumber, parentLinks.length, 'pressedNumber, parentLinks.length');
-    if (pressedNumber < parentLinks.length) {
-      const link = parentLinks[pressedNumber - 1];
-      console.log(link);
+  const pressedNumber = event.keyCode - 48;
+  if (pressedNumber > 0 && pressedNumber <= 9) {
+    const links = getLinks();
+    if (pressedNumber < links.length) {
+      const link = links[pressedNumber - 1];
       link.click();
     }
   }
