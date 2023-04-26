361fb98e3cffdfa2f961a9966181cb71da0cc005
Miika Härmä <harmamiika@gmail.com>
2023-04-26 13:39:58 +0300

    first working version

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 console.log('Hello World');
 
-const links = document.querySelectorAll('h3');
-console.log(links);
+window.onkeydown = (event) => {
+  if (document.activeElement.tagName === 'INPUT') return;
+  console.log('onkeydonw after input check');
+  // TODO: return if user has input field focused
 
-// wait for links to appear
-setTimeout(() => {
-  const links = document.querySelectorAll('h3');
-  console.log(links, 'links');
-}, 2000);
+  if (event.keyCode >= 48 && event.keyCode <= 57) {
+    console.log('number key');
+    const pressedNumber = event.keyCode - 48;
+    console.log(pressedNumber);
+
+    const links = document.querySelectorAll('a > h3');
+    console.log(links, 'links');
+    const parentLinks = Array.from(links).map((link) => link.parentElement);
+    console.log(parentLinks, 'parentLinks');
+
+    console.log(pressedNumber, parentLinks.length, 'pressedNumber, parentLinks.length');
+    if (pressedNumber < parentLinks.length) {
+      const link = parentLinks[pressedNumber - 1];
+      console.log(link);
+      link.click();
+    }
+  }
+};
